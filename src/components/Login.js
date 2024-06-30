0963eb5d646bab0c8e801de2f9348097d8fed54a
sami pevekar <sami@p.com>
2024-06-30 12:42:25 +0530

    ready to deploy

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ export default function Login(props) {
 
     const handleSubmit=async(e)=>{
         e.preventDefault()
-        const response = await fetch(`http://localhost:5000/api/auth/login`, {
+        const response = await fetch(`https://i-notebook-backend-ten.vercel.app/api/auth/login`, {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
