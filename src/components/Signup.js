2cbac41940ed5aff7e0a3bb2927e7f378257e38a
sami pevekar <sami@p.com>
2024-06-30 12:37:57 +0530

    ready to deploy

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -8,7 +8,7 @@ export default function Signup(props) {
 
   const handleSubmit=async(e)=>{
     e.preventDefault()
-    const response = await fetch(`http://localhost:5000/api/auth/createuser`, {
+    const response = await fetch(`https://i-notebook-backend-ten.vercel.app/api/auth/createuser`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,8 +2,8 @@ import NoteContext from "./NoteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-  // const host = "https://i-notebook-backend-ten.vercel.app"
-  const host = "http://localhost:5000"
+  const host = "https://i-notebook-backend-ten.vercel.app"
+  // const host = "http://localhost:5000"
   const notesInitial = []
   const [notes, setNotes] = useState(notesInitial)
 
